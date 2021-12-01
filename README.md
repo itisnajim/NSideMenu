33e4eba1a78535bae2c938735b9043f571eb83c4
Itis Najim <itisnajim@gmail.com>
2021-12-01 14:08:13 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NSideMenu
+
+A description of this package.
---
+++ Tests/NSideMenuTests/NSideMenuTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NSideMenu
+
+final class NSideMenuTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NSideMenu().text, "Hello, World!")
+    }
+}
